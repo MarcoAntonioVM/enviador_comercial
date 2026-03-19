a11009646faa5fbf7100af0a0a6902159481c4b2
Guillermo Isidoro <guillermo.isidoro.efinfo@gmail.com>
2026-03-19 10:23:37 -0600

    Add timeseries route to emailSend.routes.js for retrieving time series data from EmailSendController.

--- src/routes/emailSend.routes.js
+++ src/routes/emailSend.routes.js
@@ -6,7 +6,8 @@ const { authenticate } = require('../middlewares/auth.middleware');
 router.use(authenticate);
 
 router.get('/', emailSendController.getAll);
-router.get('/stats', emailSendController.getStats); 
+router.get('/stats', emailSendController.getStats);
+router.get('/timeseries', emailSendController.getTimeSeries);
 router.get('/:id', emailSendController.getById);
 
 module.exports = router;
